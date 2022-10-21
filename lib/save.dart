daf1b0292db4d4054eb1d526ea3f2fa12b64a561
22726020 <22726020@edu.cc.saga-u.ac.jp>
2022-10-22 00:36:48 +0900

    確認画面完成

--- lib/save.dart
+++ lib/save.dart
@@ -2,6 +2,11 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
+import 'package:gazou/owari.dart';
+import 'package:gazou/main.dart';
+import 'package:gazou/get.dart';
+
+
 class SavePage extends StatefulWidget {
   const SavePage({Key? key, required this.camera,required this.title,required this.path1, required this.path2, required this.path3}) : super(key: key);
 
@@ -24,8 +29,52 @@ class _SavePageState extends State<SavePage> {
       child:Column(
       children: <Widget>[
       Image.file(File(widget.path1)),
+      const Padding(padding: EdgeInsets.all(8)),
       Image.file(File(widget.path2)),
+      const Padding(padding: EdgeInsets.all(8)),
       Image.file(File(widget.path3)),
+
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+      ElevatedButton(
+        onPressed: (){
+          Navigator.push(context, 
+          MaterialPageRoute(builder: (context) => OwariPage(camera:widget.camera,title: "抱っこアプリ"),
+          )
+          );
+          },
+        style: ElevatedButton.styleFrom(
+          elevation: 26,
+          ),
+          child: const Text('保存する',style: TextStyle(color: Colors.black)),
+          ),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.push(context, 
+          MaterialPageRoute(builder: (context) => TakePictureScreen(camera:widget.camera),
+          )
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          elevation: 26,
+          ),
+          child: const Text('撮影をやり直す',style: TextStyle(color: Colors.black)),
+          ),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.push(context, 
+          MaterialPageRoute(builder: (context) => MyHomePage(camera:widget.camera,title: "抱っこアプリ"),
+          )
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          elevation: 26,
+          ),
+          child: const Text('ホームへ戻る',style: TextStyle(color: Colors.black)),
+          ),
+      ],
+      ),
       ],
       ),
     ),
