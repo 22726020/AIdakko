c7c850dc8ab99dde86684e223ef091f3f6954620
kouki27882663 <kouki27882663@gmail.com>
2023-06-15 20:52:51 +0900

    メモ

---
+++ lib/memo.dart
@@ -0,0 +1,13 @@
+//List<String> summraize = [];
+//[肩の平行具合、ケンダル、ヒップハンド、バックハンド、イズヒップハンド、膝までの角度(右)、腰までの角度(右)、肩までの角度(右)、膝までの角度(左)、腰までの角度(左)、肩までの角度(左)]
+
+//テキストペインター用定義
+//バックカラー指定してる
+
+//アドバイスを選択するためにsummaraizeから参照している
+// [5]right_hug_height_point)
+// [6]right_kendall_point
+// [7]right_shoulder_point)
+// [8]right_hug_height_point)
+// [9]right_kendall_point
+// [10]right_shoulder_point)
